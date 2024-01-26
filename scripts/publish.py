7eae88973f5e89258ec3f83391a8586045114858
iiPython <ben@iipython.dev>
2024-01-26 12:24:53 -0600

    Mb, wrong url

--- scripts/publish.py
+++ scripts/publish.py
@@ -2,7 +2,7 @@
 
 # Initialization
 with open("patched.js", "r") as fh:
-    data = fh.read().replace("http://localhost:8080/pub/", "https://cdn.jsdelivr.net/gh/iiPythonx/acellus/chunk/")
+    data = fh.read().replace("http://localhost:8080/pub/", "https://cdn.jsdelivr.net/gh/iiPythonx/acellus/")
 
 with open("patched_public.js", "w+") as fh:
     fh.write(data)
